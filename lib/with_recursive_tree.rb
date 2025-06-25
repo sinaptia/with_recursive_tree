75642a2855f10d2d874f7886f1a23ba22eef5fbe
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-06-25 02:54:24 +0000

    Refactor methods: methods and scopes were defined even if the class didn't represent a tree structure

--- lib/with_recursive_tree.rb
+++ lib/with_recursive_tree.rb
@@ -1,31 +1,10 @@
-require "active_support/concern"
-
 require "with_recursive_tree/version"
 
 module WithRecursiveTree
-  extend ActiveSupport::Concern
-
-  included do
-    scope :bfs, -> {
-      if defined?(ActiveRecord::ConnectionAdapters::MySQL)
-        order :depth, with_recursive_tree_order
-      else
-        order :depth
-      end
-    }
-    scope :dfs, -> do
-      if defined?(ActiveRecord::ConnectionAdapters::MySQL)
-        order with_recursive_tree_order, :path
-      elsif defined?(ActiveRecord::ConnectionAdapters::PostgreSQL)
-        order :path
-      elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3)
-        self
-      end
-    end
-  end
-
-  class_methods do
+  module ClassMethods
     def with_recursive_tree(primary_key: :id, foreign_key: :parent_id, order: nil)
+      include InstanceMethods
+
       belongs_to :parent, class_name: name, primary_key: primary_key, foreign_key: foreign_key, inverse_of: :children, optional: true
 
       has_many :children, -> { order order }, class_name: name, primary_key: primary_key, foreign_key: foreign_key, inverse_of: :parent
@@ -33,92 +12,111 @@ module WithRecursiveTree
       define_singleton_method(:with_recursive_tree_primary_key) { primary_key }
       define_singleton_method(:with_recursive_tree_foreign_key) { foreign_key }
       define_singleton_method(:with_recursive_tree_order) { order || primary_key }
-    end
-
-    def roots
-      where with_recursive_tree_foreign_key => nil
-    end
-
-    def with_recursive_tree_order_column
-      if with_recursive_tree_order.is_a?(Hash)
-        with_recursive_tree_order.keys.first
-      else
-        with_recursive_tree_order.to_s.split(" ").first
+      define_singleton_method(:with_recursive_tree_order_column) do
+        if with_recursive_tree_order.is_a?(Hash)
+          with_recursive_tree_order.keys.first
+        else
+          with_recursive_tree_order.to_s.split(" ").first
+        end
       end
+
+      scope :bfs, -> {
+        if defined?(ActiveRecord::ConnectionAdapters::MySQL)
+          order(:depth, with_recursive_tree_order)
+        else
+          order(:depth)
+        end
+      }
+      scope :dfs, -> do
+        if defined?(ActiveRecord::ConnectionAdapters::MySQL)
+          order(with_recursive_tree_order, :path)
+        elsif defined?(ActiveRecord::ConnectionAdapters::PostgreSQL)
+          order(:path)
+        elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3)
+          self
+        end
+      end
+      scope :roots, -> { where with_recursive_tree_foreign_key => nil }
     end
   end
 
-  def ancestors
-    self_and_ancestors.excluding self
-  end
-
-  def descendants
-    self_and_descendants.excluding self
-  end
-
-  def leaf?
-    children.none?
-  end
-
-  def depth
-    attributes["depth"] || ancestors.count
-  end
-  alias_method :level, :depth
-
-  def root
-    self_and_ancestors.find_by self.class.with_recursive_tree_foreign_key => nil
-  end
-
-  def root?
-    parent.blank?
-  end
-
-  def self_and_ancestors
-    self.class.with_recursive(
-      tree: [
-        self.class.where(self.class.with_recursive_tree_primary_key => send(self.class.with_recursive_tree_primary_key)),
-        self.class.joins("JOIN tree ON #{self.class.table_name}.#{self.class.with_recursive_tree_primary_key} = tree.#{self.class.with_recursive_tree_foreign_key}")
-      ]
-    ).select("*").from("tree AS #{self.class.table_name}")
-  end
-
-  def self_and_descendants
-    anchor_path = if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-      "ARRAY[#{self.class.with_recursive_tree_order_column}]::text[]"
-    elsif defined?(ActiveRecord::ConnectionAdapters::MySQL)
-      "CAST(CONCAT('/', #{self.class.with_recursive_tree_primary_key}, '/') AS CHAR(512))"
-    elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
-      "'/' || #{self.class.with_recursive_tree_primary_key} || '/'"
+  module InstanceMethods
+    def ancestors
+      self_and_ancestors.excluding self
     end
 
-    recursive_path = if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-      "tree.path || #{self.class.table_name}.#{self.class.with_recursive_tree_order_column}::text"
-    elsif defined?(ActiveRecord::ConnectionAdapters::MySQL)
-      "CONCAT(tree.path, #{self.class.table_name}.#{self.class.with_recursive_tree_primary_key}, '/')"
-    elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3)
-      "tree.path || #{self.class.table_name}.#{self.class.with_recursive_tree_primary_key} || '/'"
+    def descendants
+      self_and_descendants.excluding self
     end
 
-    recursive_query = self.class.joins("JOIN tree ON #{self.class.table_name}.#{self.class.with_recursive_tree_foreign_key} = tree.#{self.class.with_recursive_tree_primary_key}").select("#{self.class.table_name}.*, #{recursive_path} AS path, depth + 1 AS depth")
-
-    unless defined?(ActiveRecord::ConnectionAdapters::MySQL)
-      recursive_query = recursive_query.order(self.class.with_recursive_tree_order)
+    def leaf?
+      children.none?
     end
 
-    self.class.with_recursive(
-      tree: [
-        self.class.where(self.class.with_recursive_tree_primary_key => send(self.class.with_recursive_tree_primary_key)).select("*, #{anchor_path} AS path, 0 AS depth"),
-        Arel.sql(recursive_query.to_sql)
-      ]
-    ).select("*").from("tree AS #{self.class.table_name}")
+    def depth
+      attributes["depth"] || ancestors.count
+    end
+    alias_method :level, :depth
+
+    def root
+      self_and_ancestors.find_by self.class.with_recursive_tree_foreign_key => nil
+    end
+
+    def root?
+      parent.blank?
+    end
+
+    def self_and_ancestors
+      self.class.with_recursive(
+        tree: [
+          self.class.where(self.class.with_recursive_tree_primary_key => send(self.class.with_recursive_tree_primary_key)),
+          self.class.joins("JOIN tree ON #{self.class.table_name}.#{self.class.with_recursive_tree_primary_key} = tree.#{self.class.with_recursive_tree_foreign_key}")
+        ]
+      ).select("*").from("tree AS #{self.class.table_name}")
+    end
+
+    def self_and_descendants
+      anchor_path = if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+        "ARRAY[#{self.class.with_recursive_tree_order_column}]::text[]"
+      elsif defined?(ActiveRecord::ConnectionAdapters::MySQL)
+        "CAST(CONCAT('/', #{self.class.with_recursive_tree_primary_key}, '/') AS CHAR(512))"
+      elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+        "'/' || #{self.class.with_recursive_tree_primary_key} || '/'"
+      end
+
+      recursive_path = if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+        "tree.path || #{self.class.table_name}.#{self.class.with_recursive_tree_order_column}::text"
+      elsif defined?(ActiveRecord::ConnectionAdapters::MySQL)
+        "CONCAT(tree.path, #{self.class.table_name}.#{self.class.with_recursive_tree_primary_key}, '/')"
+      elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3)
+        "tree.path || #{self.class.table_name}.#{self.class.with_recursive_tree_primary_key} || '/'"
+      end
+
+      recursive_query = self.class.joins("JOIN tree ON #{self.class.table_name}.#{self.class.with_recursive_tree_foreign_key} = tree.#{self.class.with_recursive_tree_primary_key}").select("#{self.class.table_name}.*, #{recursive_path} AS path, depth + 1 AS depth")
+
+      unless defined?(ActiveRecord::ConnectionAdapters::MySQL)
+        recursive_query = recursive_query.order(self.class.with_recursive_tree_order)
+      end
+
+      self.class.with_recursive(
+        tree: [
+          self.class.where(self.class.with_recursive_tree_primary_key => send(self.class.with_recursive_tree_primary_key)).select("*, #{anchor_path} AS path, 0 AS depth"),
+          Arel.sql(recursive_query.to_sql)
+        ]
+      ).select("*").from("tree AS #{self.class.table_name}")
+    end
+
+    def self_and_siblings
+      root? ? self.class.roots : parent.children
+    end
+
+    def siblings
+      self_and_siblings.excluding self
+    end
   end
 
-  def self_and_siblings
-    root? ? self.class.roots : parent.children
-  end
-
-  def siblings
-    self_and_siblings.excluding self
+  def self.included(mod)
+    mod.extend ClassMethods
   end
 end
 
--- test/with_recursive_tree_test.rb
+++ test/with_recursive_tree_test.rb
@@ -5,11 +5,39 @@ class WithRecursiveTreeTest < ActiveSupport::TestCase
     assert WithRecursiveTree::VERSION
   end
 
-  test "it creates the parent association" do
-    assert_respond_to Node.new, :parent
+  test "adds with_recursive_tree class methods and scopes if with_recurive_tree has been called in the class definition" do
+    assert_respond_to Node, :bfs
+    assert_respond_to Node, :dfs
+    assert_respond_to Node, :roots
+    assert_respond_to Node, :with_recursive_tree_foreign_key
+    assert_respond_to Node, :with_recursive_tree_order
+    assert_respond_to Node, :with_recursive_tree_order_column
+    assert_respond_to Node, :with_recursive_tree_primary_key
   end
 
-  test "it creates the children association" do
-    assert_respond_to Node.new, :children
+  test "adds with_recursive_tree instance methods if with_recurive_tree has been called in the class definition" do
+    instance = Node.new
+
+    WithRecursiveTree::InstanceMethods.instance_methods.each do |method|
+      assert_respond_to instance, method
+    end
+  end
+
+  test "doesn't add with_recursive_tree class methods and scopes if with_recurive_tree hasn't been called in the class definition" do
+    assert_not_respond_to Person, :bfs
+    assert_not_respond_to Person, :dfs
+    assert_not_respond_to Person, :roots
+    assert_not_respond_to Person, :with_recursive_tree_foreign_key
+    assert_not_respond_to Person, :with_recursive_tree_order
+    assert_not_respond_to Person, :with_recursive_tree_order_column
+    assert_not_respond_to Person, :with_recursive_tree_primary_key
+  end
+
+  test "doesn't add with_recursive_tree instance methods if with_recurive_tree hasn't been called in the class definition" do
+    instance = Person.new
+
+    WithRecursiveTree::InstanceMethods.instance_methods.each do |method|
+      assert_not_respond_to instance, method
+    end
   end
 end
